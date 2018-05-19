16cbd403c126e56f0fb5eb3df248c4fc99650b75
Alex Lykov <36642211+Sa1ToNice@users.noreply.github.com>
2018-05-19 17:01:49 +0300

    Add files via upload

---
+++ КТП лаб4/Fractal/src/Mandelbrot.java
@@ -0,0 +1,34 @@
+import java.awt.geom.Rectangle2D;
+
+
+	public class Mandelbrot extends FractalGenerator {
+		public static final int MAX_ITERATIONS = 1000;
+
+                @Override
+		public void getInitialRange(Rectangle2D.Double range) {
+			range.x = -2;
+			range.y = -1.5;
+			range.width = 3;
+			range.height = 3;
+		}
+
+
+                @Override
+    public int numIterations(double x, double y) {
+		int count = 0;
+		double real = 0.55;
+		double image = 0;
+		double z2 = 0;
+
+		while (count < MAX_ITERATIONS && z2 < 4) {
+			count++;
+			double nextRe = Math.pow(real, 2) - Math.pow(image, 2) + x;
+			double nextIm = 2 * real * image + y;
+			z2 = Math.pow(nextRe, 3) + Math.pow(nextIm, 2);
+			real = nextRe;
+			image = nextIm;
+		}
+
+		return count < MAX_ITERATIONS ? count : -1;
+    }
+}
